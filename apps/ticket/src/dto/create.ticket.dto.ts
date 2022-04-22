8b373f3d4a01bd18ee85e282bca87abfb840198e
chavesjoabe <chavesjoabe1@gmail.com>
2022-04-22 01:16:52 -0300

    feat: create swagger docs for each api

--- apps/ticket/src/dto/create.ticket.dto.ts
+++ apps/ticket/src/dto/create.ticket.dto.ts
@@ -1,22 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTicketDto {
+  @ApiProperty({ type: 'string', example: 'Example ticket description' })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty({ type: 'string', example: 'Type Example' })
   @IsString()
   @IsNotEmpty()
   type: string;
 
+  @ApiProperty({ type: 'string', example: 'Example ticket status' })
   @IsString()
   @IsNotEmpty()
   status: string;
 
+  @ApiProperty({ type: 'string', example: 'dasd734234-13456-fghtrd-67876' })
   @IsString()
   @IsNotEmpty()
   attendatIt: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'dasd734234-13456-fghtrd-67876',
+  })
   @IsString()
   @IsNotEmpty()
   userId: string;
